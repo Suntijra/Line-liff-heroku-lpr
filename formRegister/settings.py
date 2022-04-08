6e4af9f16aa0120f8f5b318135758d68be744b88
สันติ พ่อคำจันทร์ <sunti@Macintosh.local>
2022-04-08 20:58:43 +0700

    test

--- formRegister/settings.py
+++ formRegister/settings.py
@@ -117,15 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR,'formRegister//static')]
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# print('Path =',os.path.join(BASE_DIR,'formRegister//static'))   
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'formRegister//static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+print('Path =',os.path.join(BASE_DIR,'formRegister//static'))   
